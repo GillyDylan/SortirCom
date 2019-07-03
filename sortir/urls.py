1bd02c5e51e61f439a24489d739ce72cccd3bc45
Gilly <contact@gilly-dylan.fr>
2019-07-03 09:04:50 +0200

    affichage accueil

--- sortir/urls.py
+++ sortir/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('Villes/', views.villes, name='villes'),
     path('Participants/', views.participants, name='participants'),
     path('GetSession/', views.getsession, name='session'),
+    path('GetSorties/', views.getsorties, name='sorties'),
 
 
     path('Connexion/', views.connexion, name='connexion'),
